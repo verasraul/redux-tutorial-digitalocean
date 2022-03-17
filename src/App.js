bf73dbb700d7c645da326e5f5851e3976d9e0780
Raul Veras <verasraul@gmail.com>
2022-03-16 22:34:23 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { Navbar } from './components/Navbar'
+import DashboardPage from "./pages/DashboardPage"
+import PostsPage from "./pages/PostsPage"
+import SinglePostPage from './pages/SinglePostPage'
+
+
+const App = () => {
   return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<DashboardPage />} />
+        <Route exact path="/posts" element={<PostsPage />} />
+        <Route exact path='/posts/:id' element={<SinglePostPage />} />
+        <Route path="*" />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// External imports
+// createStore, to create the store that will maintain the Redux state
+// applyMiddleware, to be able to use middleware, in this case thunk
+// Provider, to wrap the entire application in Redux
+// thunk, a middleware that allows us to make asynchronous actions in Redux
+// composeWithDevTools, code that connects your app to Redux DevTools
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-ReactDOM.render(
-  <React.StrictMode>
+// Local imports
+import App from './App'
+import rootReducer from './reducers'
+
+
+// Assets
+import './index.css'
+
+const store = createStore(rootReducer, 
+  composeWithDevTools(applyMiddleware(thunk))
+  )
+
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
+)
